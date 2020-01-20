4d8d817620e39956152d7afc3dedf9ea51d2eeb0
fabsharp <Omahabitch14>
2020-01-20 21:17:01 +0100

    refactoring

--- bin/cli.js
+++ bin/cli.js
@@ -6,19 +6,21 @@ let argv = require('minimist')(process.argv.slice(2));
 
 process.title = 'cordova-livereload';
 
-let version = require('../package.json').version
+let version = require('../package.json').version;
+let help = `
+cordova-livereload v${version}
+===========================================================
+usage : cordova-live-reload [options] <path-to-cordova-app>
+[options] :
+    -p --port          Port to use [default : 3000]
+    -h --help          Print this list and exit.
+    -v --version       Print the version and exit.'   
+<path-to-cordova-app>
+    Path to a valid cordova app [default : .]`;
+
+
 if (argv.h || argv.help) {
-    console.log([
-        '',
-        'cordova-livereload v' + version,
-        '==========================================================',
-        'usage: cordova-livereload [options] <path-to-cordova-app>',
-        '',
-        'options:',
-        '  -p --port          Port to use [3000]',
-        '  -h --help          Print this list and exit.',
-        '  -v --version       Print the version and exit.'
-    ].join('\n'));
+    console.log(help);
     process.exit();
 }
 
@@ -27,39 +29,63 @@ if (argv.v || argv.version) {
     process.exit();
 }
 
-let port = argv.p || argv.port || 3000;
-
-
-let liveReload = require('../lib/index');
-liveReload.start({
-    port : port,
-    app : argv._[0] || '.'
+const isPortAvailable = (port) => new Promise((resolve, reject) => {
+    const net = require('net');
+    const tester = net.createServer()
+        .once('error', err => reject(err))
+        .once('listening', () => tester.once('close', () => resolve()).close())
+        .listen(port);
 });
+
+const fs = require('fs');
+const isCordovaApp = (folder) => new Promise((resolve, reject) => {
+    fs.access(folder + '/config.xml', fs.F_OK, (err) => {
+        if (err) {
+            reject(err)
+        }
+        //file exists
+        resolve();
+    })
+});
+
+let port = argv.p || argv.port || 3000;
+let cordovaApp =  argv._[0] || '.';
+
+const logError = (err) => {
+    console.log(colors.red(err.message));
+    process.exit();
+};
+
+
+
+const ip = require('ip');
+const server = require("../lib/index");
+
+const process_exit = () => {
+    server.exit();
+    logError('cordova-livereload stopped');
+};
 
 if (process.platform === 'win32') {
     require('readline').createInterface({
         input: process.stdin,
         output: process.stdout
-    }).on('SIGINT', function () {
-        process.emit('SIGINT');
-    });
+    }).on('SIGINT', process_exit);
 }
 
-process.on('SIGINT', function () {
-    liveReload.exit().then(() => {
-        console.log(colors.red('cordova-livereload stopped.'));
-        process.exit();
-    }, () => {
-        process.exit();
-    })
+process.on('SIGINT', process_exit);
 
-});
+process.on('SIGTERM', process_exit);
 
-process.on('SIGTERM', function () {
-    liveReload.exit().then(() => {
-        console.log(colors.red('cordova-livereload stopped.'));
-        process.exit();
-    }, () => {
-        process.exit();
-    })
+isCordovaApp(cordovaApp).then(() => {
+    isPortAvailable(port).then(() => {
+        server.start({
+            port : port,
+            app : cordovaApp,
+            ip : ip.address()
+        })
+    }, logError);
+}, (err) => {
+    console.log(colors.red(`${cordovaApp} is not a valid cordova application : https://cordova.apache.org/#getstarted`));
+    logError(err);
 });
